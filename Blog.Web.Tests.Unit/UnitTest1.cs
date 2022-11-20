e12ba0d0977f1d12ca4958388384802162bbaf12
Grant Byrne <grant@grantbyrne.com>
2022-11-19 21:58:32 -0500

    Added in a brand new unit test project.

---
+++ Blog.Web.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Blog.Web.Tests.Unit;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
