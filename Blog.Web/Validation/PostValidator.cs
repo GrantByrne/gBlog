79cfb62a369e2ba2fde0a1e5b38fdc24464f546d
Grant Byrne <grant@grantbyrne.com>
2022-11-19 21:56:14 -0500

    Changed up the formatting on the validator.

--- Blog.Web/Validation/PostValidator.cs
+++ Blog.Web/Validation/PostValidator.cs
@@ -7,7 +7,12 @@ public class PostValidator : AbstractValidator<Post>
 {
     public PostValidator()
     {
-        RuleFor(x => x.Title).NotEmpty().WithMessage("Title is required");
-        RuleFor(x => x.Content).NotEmpty().WithMessage("Content is required");
+        RuleFor(x => x.Title)
+            .NotEmpty()
+            .WithMessage("Title is required");
+        
+        RuleFor(x => x.Content)
+            .NotEmpty()
+            .WithMessage("Content is required");
     }
 }
