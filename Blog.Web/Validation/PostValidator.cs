24c6b15362517fbdeeee9289e4ad7ccec0038fe0
Grant Byrne <grant@grantbyrne.com>
2022-11-19 21:53:26 -0500

    Added in the beginning of validation for managing a post.

---
+++ Blog.Web/Validation/PostValidator.cs
@@ -0,0 +1,13 @@
+using Blog.Web.Models;
+using FluentValidation;
+
+namespace Blog.Web.Validation;
+
+public class PostValidator : AbstractValidator<Post>
+{
+    public PostValidator()
+    {
+        RuleFor(x => x.Title).NotEmpty().WithMessage("Title is required");
+        RuleFor(x => x.Content).NotEmpty().WithMessage("Content is required");
+    }
+}
